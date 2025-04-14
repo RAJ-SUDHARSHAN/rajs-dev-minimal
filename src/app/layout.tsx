6c1f07580a0b3d7b033b1a182def80c33df04623
Raj Sudharshan Vidyalatha Natarajan <sudharshann05@gmail.com>
2025-04-13 22:28:26 -0700

    add rb2b script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -66,6 +67,9 @@ export default function RootLayout({
     <html lang="en" className={inter.className}>
       <head>
         <link rel="canonical" href="https://rajs.dev" />
+        <Script id="reb2b-script" strategy="afterInteractive">
+          {`!function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/1N5W0HMJERO5.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("1N5W0HMJERO5");}();`}
+        </Script>
       </head>
       <body suppressHydrationWarning>
         {children}
